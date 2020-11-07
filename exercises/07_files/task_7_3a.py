5d4ac254dbc1c4adc5e4820e404fbb14209f8d32
Zakharlik <zakhar.likay@mail.ru>
2020-11-07 03:36:38 +0000

    Task 7_3a accomplished

--- exercises/07_files/task_7_3a.py
+++ exercises/07_files/task_7_3a.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-vlans =dict()
+vlans = []
 with open('CAM_table.txt','r') as fil:
     for line in fil:
         if (line.startswith(' ') and line[1]!=' '):
-            vlan, mac, _, port = line.split()        
-            vlans.update({vlan: [mac,port]})
+            vlan, mac, _, port = line.split()
+            vlans.append([int(vlan), mac,port])
 vlan_list = sorted(vlans)
 for vlan in vlan_list:
-    print('{:5}{:20}{:10}'.format(vlan, vlans[vlan][0],vlans[vlan][1]))
+    print('{:<7}{:20}{:10}'.format(vlan[0], vlan[1], vlan[2]))
