8fb440acc2e48b75e7c36f4014aea6912224349b
Zakharlik <zakhar.likay@mail.ru>
2020-12-13 16:40:43 +0000

    Task 15.5 done

--- exercises/15_module_re/task_15_5.py
+++ exercises/15_module_re/task_15_5.py
@@ -24,3 +24,18 @@ description Connected to SW1 port Eth 0/1
 
 Проверить работу функции на файле sh_cdp_n_sw1.txt.
 """
+import re
+
+def generate_description_from_cdp(cdp_file):
+    result = {}
+    descr_template = "description Connected to {device} port {rem_if}"
+    with open(cdp_file, 'r') as f:
+        for line in f:
+            record = re.search('^(?P<device>\S+)\s+(?P<local_if>\S+\s\S+)\s+\d+\s+R? ?S? ?I?\s+\S+\s+(?P<rem_if>\S+\s\S+)$',line)
+            if record:
+                result[record.group('local_if')] = descr_template.format(**record.groupdict())
+    return result
+    
+if __name__ == '__main__':
+    print(generate_description_from_cdp('sh_cdp_n_sw1.txt'))
+    
