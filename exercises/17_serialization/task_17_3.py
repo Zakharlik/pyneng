44941695755a19e24bd82ad0520be033973484e2
Zakharlik <zakhar.likay@mail.ru>
2021-01-31 08:01:01 +0000

    task 17.3 done;

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,25 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+import re
+
+
+def parse_sh_cdp_neighbors(sh_cdp_ne):
+    neighbors_dict = {}
+    neighbors_list = {}
+    sh_cdp_ne = sh_cdp_ne.split('\n')
+    for line in sh_cdp_ne:
+        if '>' in line:
+            hostname = re.search('^(\S+)>',line).group(1)
+            neighbors_dict[hostname] = ''
+        if not 'Port' in line:
+            match = re.search('(?P<device>\S+)\s+(?P<local_if>\S+\s\S+)\s+\S+\S+[\S\s]+\s+\d+\s+(?P<rem_if>\S+\s\S+)',line)
+        if match:
+            neighbors_list[match.group('local_if')] = {match.group('device') : match.group('rem_if')}
+#            print(neighbors_list)
+    neighbors_dict[hostname] = neighbors_list
+    return neighbors_dict
+
+if __name__ == '__main__':
+    with open('sh_cdp_n_sw1.txt') as f:
+        print(parse_sh_cdp_neighbors(f.read()))
