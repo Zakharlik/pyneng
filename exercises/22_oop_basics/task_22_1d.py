5e46191fa96675cb97ba37a5bba74cd152a9369c
Zakharlik <zakhar.likay@mail.ru>
2021-06-02 08:54:38 +0000

    Task 22_1d done.

--- exercises/22_oop_basics/task_22_1d.py
+++ exercises/22_oop_basics/task_22_1d.py
@@ -45,6 +45,61 @@ Cоединение с одним из портов существует
 
 """
 
+
+class Topology:
+
+    def _normalize(self, topology_dict):
+        temp_topo = dict()
+        for link in topology_dict:
+            if link > topology_dict[link]:
+                temp_topo[topology_dict[link]] = link
+            else:
+                temp_topo[link] = topology_dict[link]
+        return temp_topo
+
+
+    def delete_link(self, key1, key2):
+        if self.topology.get(key1):
+            del self.topology[key1]
+        elif self.topology.get(key2):
+            del self.topology[key2]
+        else:
+            print('Такого соединения нет')
+
+
+    def delete_node(self, node):
+        del_nodes = []
+        for link in self.topology:
+            if (link[0] == node) | (self.topology[link][0] == node):
+                del_nodes.append(link)
+        if del_nodes:
+            for node in del_nodes:
+                del self.topology[node]
+        else:
+            print('Такого устройства нет')
+
+    def add_link(self, side1, side2):
+        one_side = False
+        link_present = False
+        for link in self.topology:
+            if ((link == side1) and (self.topology[link] == side2)) or ((link == side2) and (self.topology[link] == side1)):
+                link_present = True
+            elif (link == side1) or (self.topology[link] == side2) or (link == side2) or (self.topology[link] == side1):
+                one_side = True
+        if link_present:
+            print('Такое соединение существует')
+        elif one_side:
+            print('Cоединение с одним из портов существует')
+        else:
+            self.topology[side1] = side2
+
+
+
+
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
     ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
@@ -56,3 +111,10 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    top.add_link(('R1', 'Eth0/4'), ('R7', 'Eth0/0'))
+    top.add_link(('R1', 'Eth0/4'), ('R7', 'Eth0/0'))
+    top.add_link(('R1', 'Eth0/4'), ('R5', 'Eth5/0'))
