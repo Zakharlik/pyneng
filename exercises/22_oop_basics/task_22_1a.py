c7ec5a49859786d2fadfae37f42a7c4a0f2c68ec
Zakharlik <zakhar.likay@mail.ru>
2021-06-02 07:22:42 +0000

    Task 22a done

---
+++ exercises/22_oop_basics/task_22_1a.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+Задание 22.1a
+
+Скопировать класс Topology из задания 22.1 и изменить его.
+
+Перенести функциональность удаления "дублей" в метод _normalize.
+При этом метод __init__ должен выглядеть таким образом:
+"""
+
+class Topology:
+    def __init__(self, topology):
+        temp_topo = dict()
+        for link in topology:
+            if link > topology[link]:
+                temp_topo[topology[link]] = link
+            else:
+                temp_topo[link] = topology[link]
+        self.topology = temp_topo
+
+
+
+class Topology:
+
+    def _normalize(self, topology_dict):
+        temp_topo = dict()
+        for link in topology_dict:
+            if link > topology_dict[link]:
+                temp_topo[topology_dict[link]] = link
+            else:
+                temp_topo[link] = topology_dict[link]
+        return temp_topo
+
+
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    
+
+topology_example = {
+    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+}
+
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    print(top.topology)
