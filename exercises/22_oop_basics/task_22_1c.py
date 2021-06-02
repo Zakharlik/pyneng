999e8921ee880f53e74c7569cc2d3110f5477643
Zakharlik <zakhar.likay@mail.ru>
2021-06-02 08:40:50 +0000

    Task 22_1c done.

--- exercises/22_oop_basics/task_22_1c.py
+++ exercises/22_oop_basics/task_22_1c.py
@@ -36,6 +36,45 @@ In [5]: t.delete_node('SW1')
 
 """
 
+
+
+class Topology:
+
+    def _normalize(self, topology_dict):
+        temp_topo = dict()
+        for link in topology_dict:
+            if link > topology_dict[link]:
+                temp_topo[topology_dict[link]] = link
+            else:
+                temp_topo[link] = topology_dict[link]
+        return temp_topo
+
+
+    def delete_link(self, key1, key2):
+        if self.topology.get(key1):
+            del self.topology[key1]
+        elif self.topology.get(key2):
+            del self.topology[key2]
+        else:
+            print('Такого соединения нет')
+
+
+    def delete_node(self, node):
+        del_nodes = []
+        for link in self.topology:
+            if (link[0] == node) | (self.topology[link][0] == node):
+                del_nodes.append(link)
+        if del_nodes:
+            for node in del_nodes:
+                del self.topology[node]
+        else:
+            print('Такого устройства нет')
+
+
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
     ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
@@ -47,3 +86,14 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    print(top.topology)
+    top.delete_link(('R5', 'Eth0/0'), ('R3', 'Eth0/2'))
+    print(top.topology)
+    top.delete_node('SW1')
+    print(top.topology)
+    top.delete_node('SW1')
+   
