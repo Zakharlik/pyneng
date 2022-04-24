b001e30f9e53c1c650bd13f5c07f75bddd95f2fb
Zakhar Likay <zakhar.likay@mail.ru>
2022-04-24 22:49:30 +0700

    task 23_3 done but not tested

--- exercises/23_oop_special_methods/task_23_3.py
+++ exercises/23_oop_special_methods/task_23_3.py
@@ -75,3 +75,64 @@ topology_example2 = {
     ("R1", "Eth0/4"): ("R7", "Eth0/0"),
     ("R1", "Eth0/6"): ("R9", "Eth0/0"),
 }
+
+
+class Topology:
+
+    def _normalize(self, topology_dict):
+        temp_topo = dict()
+        for link in topology_dict:
+            if link > topology_dict[link]:
+                temp_topo[topology_dict[link]] = link
+            else:
+                temp_topo[link] = topology_dict[link]
+        return temp_topo
+
+    def delete_link(self, key1, key2):
+        if self.topology.get(key1):
+            del self.topology[key1]
+        elif self.topology.get(key2):
+            del self.topology[key2]
+        else:
+            print('Такого соединения нет')
+
+    def delete_node(self, node):
+        del_nodes = []
+        for link in self.topology:
+            if (link[0] == node) | (self.topology[link][0] == node):
+                del_nodes.append(link)
+        if del_nodes:
+            for node in del_nodes:
+                del self.topology[node]
+        else:
+            print('Такого устройства нет')
+
+    def add_link(self, side1, side2):
+        one_side = False
+        link_present = False
+        for link in self.topology:
+            if ((link == side1) and (self.topology[link] == side2)) or ((link == side2) and (self.topology[link] == side1)):
+                link_present = True
+            elif (link == side1) or (self.topology[link] == side2) or (link == side2) or (self.topology[link] == side1):
+                one_side = True
+        if link_present:
+            print('Такое соединение существует')
+        elif one_side:
+            print('Cоединение с одним из портов существует')
+        else:
+            self.topology[side1] = side2
+
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def __add__(self, t2):
+        temp_topo = t2.topology
+        for link in temp_topo:
+            self.topology[link] = temp_topo[link]
+        return self.topology
+
+if __name__ == '__main__':
+    t1 = Topology(topology_example)
+    t2 = Topology(topology_example2)
+    t3 = t1 + t2
+    print(t3)
