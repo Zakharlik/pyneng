d8f2f0d2b745ebc3bd0a087f4d5b1244b371a631
Zakhar Likay <zakhar.likay@mail.ru>
2022-04-24 22:13:36 +0700

    task 23.1 done, but not tested.

--- exercises/23_oop_special_methods/task_23_1.py
+++ exercises/23_oop_special_methods/task_23_1.py
@@ -43,3 +43,43 @@ In [6]: ip1 = IPAddress('10.1.1.1/240')
 ValueError: Incorrect mask
 
 """
+class IPAddres:
+   def isIpCorrct(self, ipAddress):
+      octets = ipAddress.split('.')
+      isIpCorrect = True;
+      if len(octets) == 4:
+         for octet in octets:
+            try:
+               if int(octet) < 0 or int(octet) > 255:
+                  isIpCorrect = False
+            except ValueError:
+               raise ValueError('Incorrect IPv4 address')
+      else:
+         raise ValueError('Incorrect IPv4 address')
+      if not isIpCorrect:
+         raise ValueError('Incorrect IPv4 address')
+      return True
+
+   def isMaskCorrect(self, mask):
+      try:
+         if int(mask) < 8 or int(mask) > 24:
+            raise ValueError('Incorrect mask')
+      except:
+         raise ValueError('Incorrect mask')
+      print('Mask checked')
+      return True
+   
+   def isAddressMaskCorrect(self, ipAddressMask):
+      self.ip, self.mask = ipAddressMask.split('/')
+      if self.isIpCorrct(self.ip) and self.isMaskCorrect(self.mask):
+         return True
+
+   def __init__(self, ipAddressMask):
+      res = self.isAddressMaskCorrect(ipAddressMask)
+       
+
+
+if __name__ == "__main__":
+   ip1 = IPAddres('100.1.1.1/240')
+   print(ip1.ip)
+   print(ip1.mask)
