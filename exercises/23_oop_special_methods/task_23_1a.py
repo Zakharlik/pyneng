f334533d8555c59fc237951aca391c19593753b7
Zakhar Likay <zakhar.likay@mail.ru>
2022-04-24 22:33:10 +0700

    task 23_1a done, but not tested

--- exercises/23_oop_special_methods/task_23_1a.py
+++ exercises/23_oop_special_methods/task_23_1a.py
@@ -31,3 +31,49 @@ In [12]: print(ip_list)
 [IPAddress('10.1.1.1/24')]
 
 """
+class IPAddres:
+  def isIpCorrct(self, ipAddress):
+    octets = ipAddress.split('.')
+    isIpCorrect = True;
+    if len(octets) == 4:
+      for octet in octets:
+        try:
+          if int(octet) < 0 or int(octet) > 255:
+            isIpCorrect = False
+        except ValueError:
+          raise ValueError('Incorrect IPv4 address')
+    else:
+      raise ValueError('Incorrect IPv4 address')
+    if not isIpCorrect:
+      raise ValueError('Incorrect IPv4 address')
+    return True
+
+  def isMaskCorrect(self, mask):
+    try:
+      if int(mask) < 8 or int(mask) > 24:
+        raise ValueError('Incorrect mask')
+    except:
+      raise ValueError('Incorrect mask')
+    print('Mask checked')
+    return True
+
+  def isAddressMaskCorrect(self, ipAddressMask):
+    self.ip, self.mask = ipAddressMask.split('/')
+    if self.isIpCorrct(self.ip) and self.isMaskCorrect(self.mask):
+      return True
+
+  def __init__(self, ipAddressMask):
+    res = self.isAddressMaskCorrect(ipAddressMask)
+
+  def __str__(self) -> str:
+      return f'IP address {self.ip}/{self.mask}'
+  
+  def __repr__(self) -> str:
+      return f"IP address('{self.ip}/{self.mask}')"
+
+if __name__ == "__main__":
+  ip1 = IPAddres('100.1.1.1/24')
+  print(ip1)
+  ip_list = []
+  ip_list.append(ip1)
+  print(ip_list)
